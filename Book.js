3d9abd18cd06c21a87ba9d74d65bf8a7e5ace16a
JeyanthiKadalkani <jeyanthi_kadalkani@outlook.com>
2019-05-13 18:09:27 +0530

    typescript

---
+++ Book.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Book {
+    constructor(bookNumber, bookName, author, ratePerUnit) {
+        this.bookNumber = bookNumber;
+        this.bookName = bookName;
+        this.author = author;
+        this.ratePerUnit = ratePerUnit;
+    }
+    toString() {
+        return `${this.bookName},${this.author}`;
+    }
+}
+exports.default = Book;
+exports.authorList = ["Siva", "Pavan", "Shashi", "Magesh", "Mahi"];
